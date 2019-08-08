ee479ac5a7dd453bae96e5263a026d9da55d9709
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-08-08 21:10:44 +0700

    add models

---
+++ app/src/main/java/com/example/cats/models/Cat.java
@@ -0,0 +1,7 @@
+package com.example.cats.models;
+
+public class Cat {
+    public String name;
+    public String image;
+    public String description;
+}
---
+++ app/src/main/java/com/example/cats/models/CatsData.java
@@ -0,0 +1,8 @@
+package com.example.cats.models;
+
+import java.util.ArrayList;
+
+public class CatsData {
+
+    public ArrayList<Cat> listData
+}
