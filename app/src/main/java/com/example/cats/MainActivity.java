d70594ddc6d5cb39444ad686c0aa2b70da722292
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-08-11 03:34:41 +0700

    update main activity

--- app/src/main/java/com/example/cats/MainActivity.java
+++ app/src/main/java/com/example/cats/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.cats;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.cats.models.Cat;
+import com.example.cats.models.CatsData;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerViewCats;
+    private ArrayList<Cat> catList = new ArrayList<>();
+    private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewCats = findViewById(R.id.rv_cat);
+        recyclerViewCats.setHasFixedSize(true);
+
+        catList.addAll(CatsData.getArrayList());
+        Log.d(TAG, "onCreate: " + catList.toString());
     }
 }
