d1e768ef7ebc317c4e76888ccc66eb95e57b5fcf
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-08-11 03:34:30 +0700

    add cat data model

--- app/src/main/java/com/example/cats/models/CatsData.java
+++ app/src/main/java/com/example/cats/models/CatsData.java
@@ -4,5 +4,10 @@ import java.util.ArrayList;
 
 public class CatsData {
 
-    public ArrayList<Cat> listData
+    public static ArrayList<Cat> arrayList = new ArrayList<Cat>();
+
+    public static ArrayList<Cat> getArrayList() {
+        arrayList.add(new Cat("Abyssinian", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Gustav_chocolate.jpg/220px-Gustav_chocolate.jpg", "Egypt", "14 - 15", "The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals."));
+        return arrayList;
+    }
 }
