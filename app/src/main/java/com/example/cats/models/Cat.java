b9fda9ad649f6558a732ac58d3b0b674122a02db
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-08-11 03:34:21 +0700

    add cat model

--- app/src/main/java/com/example/cats/models/Cat.java
+++ app/src/main/java/com/example/cats/models/Cat.java
@@ -3,5 +3,66 @@ package com.example.cats.models;
 public class Cat {
     public String name;
     public String image;
+    public String origin;
+    public String lifeSpan;
     public String description;
+
+    public Cat(String name, String image, String origin, String lifeSpan, String description) {
+        this.name = name;
+        this.image = image;
+        this.origin = origin;
+        this.lifeSpan = lifeSpan;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", image='" + image + '\'' +
+                ", origin='" + origin + '\'' +
+                ", lifeSpan='" + lifeSpan + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getLifeSpan() {
+        return lifeSpan;
+    }
+
+    public void setLifeSpan(String lifeSpan) {
+        this.lifeSpan = lifeSpan;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
