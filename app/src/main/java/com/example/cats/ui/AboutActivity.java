cda9b08108100a13628e213c2dd9aefeac559f65
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-08-12 02:17:54 +0700

    update about activity

--- app/src/main/java/com/example/cats/ui/AboutActivity.java
+++ app/src/main/java/com/example/cats/ui/AboutActivity.java
@@ -7,15 +7,27 @@ package com.example.cats.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.cats.R;
 
 public class AboutActivity extends AppCompatActivity {
+    ImageView imgAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        imgAbout = findViewById(R.id.img_about);
+
+        Glide.with(this)
+                .load(Uri.parse("file:///android_asset/images/arif_ikhsanudin.jpg"))
+                .apply(new RequestOptions().override(200, 200))
+                .into(imgAbout);
     }
 }
