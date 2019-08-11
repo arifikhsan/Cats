3e4f17ceb3792793f6f054e124b9880b7b430d13
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-08-11 11:13:22 +0700

    add image

--- app/src/main/java/com/example/cats/ui/CatDetailActivity.java
+++ app/src/main/java/com/example/cats/ui/CatDetailActivity.java
@@ -1,16 +1,33 @@
 package com.example.cats.ui;
 
+import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.cats.R;
+import com.example.cats.models.Cat;
 
 public class CatDetailActivity extends AppCompatActivity {
+    public static final String EXTRA_CAT = "extra_cat";
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat_detail);
+
+        imageView = findViewById(R.id.img_cat_detail);
+
+        Cat cat = getIntent().getParcelableExtra(EXTRA_CAT);
+        if (cat != null) {
+            Glide.with(this)
+                    .load(cat.getImage())
+                    .apply(new RequestOptions().override(200, 300))
+                    .into(imageView);
+        }
     }
 }
