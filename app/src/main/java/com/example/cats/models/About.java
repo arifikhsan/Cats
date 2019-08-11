df1e68ebb1fec4ac8b4fc4369dbaff268e79efda
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-08-11 10:48:30 +0700

    add files

---
+++ app/src/main/java/com/example/cats/models/About.java
@@ -0,0 +1,4 @@
+package com.example.cats.models;
+
+public class About {
+}
---
+++ app/src/main/java/com/example/cats/ui/CatDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.cats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CatDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cat_detail);
+    }
+}
