c76518ea42641363ddbcc614608a1eefc97906f4
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-08-11 10:48:55 +0700

    add about data

--- app/src/main/java/com/example/cats/data/AboutData.java
+++ app/src/main/java/com/example/cats/data/AboutData.java
@@ -1,4 +1,14 @@
 package com.example.cats.data;
 
+import com.example.cats.models.About;
+
 public class AboutData {
+    public static About about;
+
+    public static About getAbout() {
+        about.setName("Arif Ikhsanudin");
+        about.setImage("");
+        about.setEmail("arif.ikhsanudin.developer@gmail.com");
+        return about;
+    }
 }
