d5979dac5bd144e91649d7f437d0b800e9a78f54
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-08-11 10:49:25 +0700

    fix import R

--- app/src/main/java/com/example/cats/ui/CatDetailActivity.java
+++ app/src/main/java/com/example/cats/ui/CatDetailActivity.java
@@ -1,8 +1,10 @@
-package com.example.cats;
+package com.example.cats.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.cats.R;
 
 public class CatDetailActivity extends AppCompatActivity {
 
