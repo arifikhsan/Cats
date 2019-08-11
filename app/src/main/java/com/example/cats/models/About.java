ff9f0180fd5fa7506ab6188300f0c8fdd88e901a
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-08-11 10:49:03 +0700

    add about model

--- app/src/main/java/com/example/cats/models/About.java
+++ app/src/main/java/com/example/cats/models/About.java
@@ -1,4 +1,31 @@
 package com.example.cats.models;
 
 public class About {
+    public String name;
+    public String image;
+    public String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
