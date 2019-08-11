643ad366cb964dfd5ff94fd5fb7e535c8363c0cd
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-08-11 10:24:16 +0700

    add onclick

--- app/src/main/java/com/example/cats/MainActivity.java
+++ app/src/main/java/com/example/cats/MainActivity.java
@@ -2,18 +2,21 @@ package com.example.cats;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.cats.adapters.ListCatAdapter;
 import com.example.cats.models.Cat;
-import com.example.cats.models.CatsData;
+import com.example.cats.data.CatsData;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView recyclerViewCats;
-    private ArrayList<Cat> catList = new ArrayList<>();
+    private RecyclerView rvCats;
+    private ArrayList<Cat> cats = new ArrayList<>();
     private String TAG = "MainActivity";
 
     @Override
@@ -21,10 +24,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerViewCats = findViewById(R.id.rv_cat);
-        recyclerViewCats.setHasFixedSize(true);
+        rvCats = findViewById(R.id.rv_cat);
+        rvCats.setHasFixedSize(true);
 
-        catList.addAll(CatsData.getArrayList());
-        Log.d(TAG, "onCreate: " + catList.toString());
+        cats.addAll(CatsData.getArrayList());
+        Log.d(TAG, "onCreate: " + cats.toString());
+        showRecyclerList();
+    }
+
+    private void showRecyclerList() {
+        rvCats.setLayoutManager(new LinearLayoutManager(this));
+        ListCatAdapter listCatAdapter = new ListCatAdapter(cats);
+        rvCats.setAdapter(listCatAdapter);
+
+        listCatAdapter.setOnItemClickCallback(new ListCatAdapter.OnItemClickCallback() {
+            @Override
+            public void onItemClicked(Cat cat) {
+                moveToCatDetailActivity(cat);
+            }
+        });
+    }
+
+    private void moveToCatDetailActivity(Cat cat) {
+        Toast.makeText(this, cat.toString(), Toast.LENGTH_SHORT).show();
     }
 }
