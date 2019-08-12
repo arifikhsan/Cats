8d13630d2c300542157c31a45e756c1d232bc525
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-08-12 22:45:22 +0700

    add base activity

--- app/src/main/java/com/example/cats/base/BaseActivity.java
+++ app/src/main/java/com/example/cats/base/BaseActivity.java
@@ -6,5 +6,27 @@
 package com.example.cats.base;
 // Created by Arif Ikhsanudin on 8/12/2019.
 
-public interface BaseActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResourceId());
+        setToolbar();
+        initView();
+        populateView();
+    }
+
+    protected abstract int getLayoutResourceId();
+
+    protected abstract void setToolbar();
+
+    protected abstract void initView();
+
+    protected abstract void populateView();
 }
