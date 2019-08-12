15dd5083e642e63f690fc7358c31f0b849d4189f
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-08-12 15:24:13 +0700

    add base activity

---
+++ app/src/main/java/com/example/cats/adapters/ListTemperamentAdapter.java
@@ -0,0 +1,5 @@
+package com.example.cats.adapters;
+// Created by Arif Ikhsanudin on 8/12/2019.
+
+public class ListTemperamentAdapter {
+}
---
+++ app/src/main/java/com/example/cats/base/BaseActivity.java
@@ -0,0 +1,5 @@
+package com.example.cats.base;
+// Created by Arif Ikhsanudin on 8/12/2019.
+
+public interface BaseActivity {
+}
